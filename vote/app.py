d46d5d2ac703b1dd45d8da29b78776dcd16d3638
yannick <yannickparker84@gmail.com>
2022-07-06 13:17:15 -0500

    kdk

---
+++ vote/app.py
@@ -0,0 +1,40 @@
+import os
+import random
+import socket
+import requests
+
+from flask import Flask, g, make_response, render_template, request
+
+option_a = os.getenv('OPTION_A', "Cats")
+option_b = os.getenv('OPTION_B', "Dogs")
+hostname = socket.gethostname()
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST','GET'])
+def hello():
+    voter_id = request.cookies.get('voter_id')
+    if not voter_id:
+        voter_id = hex(random.getrandbits(64))[2:-1]
+
+    vote = None
+
+    if request.method == 'POST':
+        vote = request.form['vote']
+        req = {"voter_id": voter_id, "vote": vote}
+        r = requests.post("http://gateway:8080/function/func_process_vote", json=req)
+        print(str(r.status_code))
+
+    resp = make_response(render_template(
+        'index.html',
+        option_a=option_a,
+        option_b=option_b,
+        hostname=hostname,
+        vote=vote,
+    ))
+    resp.set_cookie('voter_id', voter_id)
+    return resp
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
---
+++ vote/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker build -t alexellis2/faas-vote .
+
+echo alexellis2/faas-vote
